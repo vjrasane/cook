c1e898c358205573f2ff8180a95edef65590be40
vjrasane <3148501+vjrasane@users.noreply.github.com>
2026-02-09 10:40:04 +0200

    devenv

---
+++ devenv.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+{
+  dotenv.enable = true;
+
+  env = {
+  };
+
+  packages = with pkgs; [
+    cook-cli
+  ];
+
+  git-hooks.hooks = {
+    nixfmt.enable = true;
+    check-shebang-scripts-are-executable.enable = true;
+    check-symlinks.enable = true;
+    check-yaml.enable = true;
+    ripsecrets.enable = true;
+    shellcheck.enable = true;
+    shfmt.enable = true;
+    trim-trailing-whitespace.enable = true;
+    end-of-file-fixer.enable = true;
+  };
+
+}
